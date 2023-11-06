117199972ad7cadcb1ad17e79e10ff464e67ec1e
hdpe123 <hdpe@outlook.com>
2023-11-06 11:19:17 -0500

    Submitted version. App won't run continuously.

--- app/src/main/java/cst7335/platformjumpgame/MainActivity.java
+++ app/src/main/java/cst7335/platformjumpgame/MainActivity.java
@@ -5,10 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private GameView gameView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get the GameView instance from the layout
+        gameView = findViewById(R.id.game_view);
+
+        // Start run() thread
+        gameView.resume();
     }
 }
